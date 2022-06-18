a9c65a794101ddcb75ef205c736b270564f0d2ce
Kamil-Lontkowski <kamillont14@gmail.com>
2022-06-19 00:43:55 +0200

    Added handlers for seekTo, set, play and pause

--- backend/src/socketio/movieHandler.ts
+++ backend/src/socketio/movieHandler.ts
@@ -4,6 +4,7 @@ import { ServerToClientEvents } from '../events/serverToClientEvents';
 import { ClientToServerEvents } from '../events/clientToServerEvents';
 import { IRoomService } from '../services/IRoomService';
 import { IUserService } from '../services/IUserService';
+import { Movie } from '../common/Movie';
 
 export const registerMovieHandlers = async (
   io: Server<ClientToServerEvents, ServerToClientEvents>,
@@ -19,7 +20,40 @@ export const registerMovieHandlers = async (
         return Promise.resolve();
       } else {
         room.movie.currentProggres = progress;
-        console.log('Progess ' + progress);
+        io.to(roomId).emit('onMovieChange', room.movie);
+      }
+    }
+  });
+  await socket.on('setMovie', async (ytMovieId) => {
+    const roomId = roomService.getParticipantRoomId(socket.id);
+    const room = roomService.getRoomById(roomId);
+    const movie = new Movie(ytMovieId, false, 0);
+    if (room == undefined) {
+      return Promise.resolve();
+    } else {
+      room.movie = movie;
+      io.to(roomId).emit('onMovieChange', movie);
+    }
+  });
+  await socket.on('playMovie', async () => {
+    const roomId = roomService.getParticipantRoomId(socket.id);
+    const room = roomService.getRoomById(roomId);
+    if (room != undefined) {
+      if (room.movie == undefined) {
+        return Promise.resolve();
+      } else {
+        room.movie.isPlaying = true;
+        io.to(roomId).emit('onMovieChange', room.movie);
+      }
+    }
+  });
+  await socket.on('pouseMovie', async () => {
+    const roomId = roomService.getParticipantRoomId(socket.id);
+    const room = roomService.getRoomById(roomId);
+    if (room != undefined) {
+      if (room.movie == undefined) {
+        return Promise.resolve();
+      } else {
         console.log(room.movie);
         io.to(roomId).emit('onMovieChange', room.movie);
       }
